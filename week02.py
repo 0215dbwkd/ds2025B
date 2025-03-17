7062bb99f6df824cbc075acc7ec0d7a5b2f7933f
0215dbwkd <0215dbwkd@naver.com>
2025-03-17 11:31:07 +0900

    0.1 Big O, O(n) & README.md & .gitignore

---
+++ week02.py
@@ -0,0 +1,11 @@
+# 실행 할 때는 alt + shift + F10
+# 재실행은 ctrl + F5
+
+n = int(input("정수 입력 : "))
+result = 0
+for i in range(1, n+1):
+    result = result + i
+#result = n * (n + 1) // 2
+print(result)
+
+# O(n)
