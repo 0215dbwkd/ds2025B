50645f1359b4ae23f18951c24f5f3634c1752d04
0215dbwkd <0215dbwkd@naver.com>
2025-03-17 20:26:02 +0900

    FinalVersionProject

--- week2_project.py
+++ week2_project.py
@@ -1,32 +1,29 @@
 import random
 
 answer = random.randint(1, 100)
-win = False
+count = 0
 
-for guesses in range(3, 0, -1):
-
+while True:
+    count += 1
     while True:
-        guess = input(f"{guesses}번의 기회가 남았습니다. 숫자 입력 : ")
+        guess = input(f"숫자 입력 : ")
 
         try:
             guess = int(guess)
             break
         except:
             print("숫자를 입력해주세요")
+            print("")
 
     print(guess)
 
     if answer == guess:
-        print("정답입니다!")
+        print(f"정답입니다! 당신은 총 {count}번 만에 맞췄습니다.")
         print("")
-        win = True
         break
     elif answer > guess:
         print("입력하신 값은 정답보다 작은 수 입니다. LOW")
     else:
         print("입력하신 값은 정답보다 큰 수 입니다 HIGH.")
 
-if win:
-    print("당신이 이겼습니다!")
-else:
-    print(f"당신이 졌습니다. 정답은 {answer}입니다.")
+
