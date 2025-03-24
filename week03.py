a5f6d3cb86d9a4c2056662ea5f5dbe2e06015ca2
0215dbwkd <0215dbwkd@naver.com>
2025-03-24 10:46:44 +0900

    v0.4 array

---
+++ week03.py
@@ -0,0 +1,10 @@
+array = [11, 9, -77, 8]
+
+for i in range(len(array)):
+    print(f"{array[i]:3} {id(array[i])}")
+
+
+#print(f"{array[0]:3} {id(array[0])}")
+#print(f"{array[1]:3} {id(array[1])}")
+#print(f"{array[2]:3} {id(array[2])}")
+#print(f"{array[3]:3} {id(array[3])}")
