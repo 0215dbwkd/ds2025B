c4b08391a3db64df3aca9e32b127cc493c80bf98
0215dbwkd <0215dbwkd@naver.com>
2025-04-02 10:49:24 +0900

    v1.7 linked list

--- week04.py
+++ week04.py
@@ -40,6 +40,28 @@ class LinkedList:
                 current = current.link
         return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
 
+    def reverse_list(self):
+        current = self.head
+        previous = None
+        while current:
+            next_node = current.link
+            current.link = previous
+            previous = current
+            current = next_node
+        self.head = previous
+
+    def detect_cycle(self):
+        slow = self.head
+        fast = self.head
+        while True:
+            try:
+                slow = slow.link
+                fast = fast.link.link
+                if slow is fast:
+                    return True
+            except:
+                return False
+
     def __str__(self):
         node = self.head
         out_texts = ""
@@ -54,8 +76,16 @@ ll.append(8)
 ll.append(10)
 ll.append(-9)
 print(ll)
+
 print(ll.search(100))
 print(ll.search(10))
+
+print(ll.detect_cycle())
+# ll.head.link.link.link = ll.head  # 3번 노드가 1번 노드를 가리킨다
+# print(ll.detect_cycle())
 ll.remove(90)
 ll.remove(10)
+print(ll)
+
+ll.reverse_list()
 print(ll)
