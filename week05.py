4bd16cc7c075f482788c766dc0beac14b2574d8c
0215dbwkd <0215dbwkd@naver.com>
2025-04-07 11:11:58 +0900

    v2.0 stack

--- week05.py
+++ week05.py
@@ -1,1 +1,31 @@
-.
+class Stack:
+    def __init__(self):
+        self.items =list()
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        return self.items[-1]
+
+s1 = Stack()
+s2 = Stack()
+print(s1.is_empty())
+s1.push("자료구조")
+print(s1.is_empty())
+print(s2.is_empty())
+s1.push("데이터베이스")
+print(s1.size())
+print(s1.peek())  # 마지막 원소를 삭제하지 않음
+print(s1.size())
+print(s1.pop()) # 삭제함
+print(s1.size())
