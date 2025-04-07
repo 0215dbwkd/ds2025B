20c65712986d7bd95f452da139d2700e91d539c6
0215dbwkd <0215dbwkd@naver.com>
2025-04-07 12:19:20 +0900

    v2.4 Stack

--- week05.py
+++ week05.py
@@ -1,13 +1,16 @@
-s1 = list()
-print(len(s1))
-s1.append("Data structure")  # push
-s1.append("DataBase")  # push
-print(len(s1))  # size
-print(s1[-1])  # peek
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
-# print(s1.pop())
-# print(s1)
+def check_parentheses(expression : str) -> bool: # type hint
+    stack = []
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if(len(stack) == 0):
+                return False
+            else:
+                stack.pop() #집어 넣은 걸 뺀다
+    return len(stack) == 0
+
+print(check_parentheses("(2+3)"))
+print(check_parentheses("(2+(2*3))"))
+print(check_parentheses("(2+(2*3)")) # 삽입 2번 팝 1번 stack에 여는 소괄호 하나가 남아서 False
+print(check_parentheses(")2+(2*3)("))
