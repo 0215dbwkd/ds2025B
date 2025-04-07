db5d7e1aba5af8948bb9b09dc6fecf259687d137
0215dbwkd <0215dbwkd@naver.com>
2025-04-07 10:44:25 +0900

    v1.9 linked list Bug fix

--- week04.py
+++ week04.py
@@ -1,5 +1,3 @@
-import random
-
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -10,6 +8,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+
     def append(self, data):
         if not self.head:
             self.head = Node(data)
@@ -19,17 +18,22 @@ class LinkedList:
             current = current.link
         current.link = Node(data)
 
+
     def remove(self, target):
+        current = self.head
         if self.head.data == target:
             self.head = self.head.link
+            current.link = None
             return
-        current = self.head
         previous = None
         while current:
             if current.data == target:
-                previous.link = current.link #삭제 역할
+                previous.link = current.link
+                current.link = None
+                break
             previous = current
             current = current.link
+
 
     def search(self, target):
         current = self.head
@@ -40,27 +44,6 @@ class LinkedList:
                 current = current.link
         return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
 
-    def reverse_list(self):
-        current = self.head
-        previous = None
-        while current:
-            next_node = current.link
-            current.link = previous
-            previous = current
-            current = next_node
-        self.head = previous
-
-    def detect_cycle(self):
-        slow = self.head
-        fast = self.head
-        while True:
-            try:
-                slow = slow.link
-                fast = fast.link.link
-                if slow is fast:
-                    return True
-            except:
-                return False
 
     def __str__(self):
         node = self.head
@@ -76,16 +59,9 @@ ll.append(8)
 ll.append(10)
 ll.append(-9)
 print(ll)
-
 print(ll.search(100))
-print(ll.search(10))
-
-print(ll.detect_cycle())
-# ll.head.link.link.link = ll.head  # 3번 노드가 1번 노드를 가리킨다
-# print(ll.detect_cycle())
+print(ll.search(-9))
 ll.remove(90)
-ll.remove(10)
-print(ll)
-
-ll.reverse_list()
+# ll.remove(-9)
+ll.remove(8)
 print(ll)
